51eb0674f7cff09a6f0664467422f10584d9898d
pasqualedimaro <pakydm15@gmail.com>
2025-06-25 15:54:43 +0200

    fix and rename names and hobbies

--- hobbies_js/script.js
+++
@@ -1,7 +0,0 @@
-// scrivo la costante con require 
-
-const hobbies = require("./function.js");
-
-// do i valori agli hobby
-
-console.log(hobbies("giocare ai videogames", "leggere manga", "ascoltare musica"));
--- names_js/script.js
+++
@@ -1,6 +0,0 @@
-//scrivo la costante con require
-
-const fullname = require("./function.js");
-
-// do i valori a firstName e lastName
-console.log(fullname("Pasquale", "Di Maro"));
