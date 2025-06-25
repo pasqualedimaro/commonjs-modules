8491d0ed10565e716014b523947771a1edcbc140
pasqualedimaro <pakydm15@gmail.com>
2025-06-25 15:28:28 +0200

    complete names.js

---
+++ names_js/function.js
@@ -0,0 +1,12 @@
+// creo la funzione che da due parametri nome e cognome
+
+function fullname(firstName, lastName) {
+    return{
+        firstName, lastName
+    };
+    
+}
+
+// esporto la funzione
+
+module.exports = fullname;
---
+++ names_js/script.js
@@ -0,0 +1,6 @@
+//scrivo la costante con require
+
+const fullname = require("./function.js");
+
+// do i valori a firstName e lastName
+console.log(fullname("Pasquale", "Di Maro"));
