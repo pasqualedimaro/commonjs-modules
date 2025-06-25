f3d462d5b8a04f0bb691dcddb1af276bed1945b2
pasqualedimaro <pakydm15@gmail.com>
2025-06-25 15:38:53 +0200

    complete hobbies.js

---
+++ hobbies_js/function.js
@@ -0,0 +1,12 @@
+// scrivo la funzione con tre parametri
+
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return{
+        hobbyOne, hobbyTwo, hobbyThree
+    };
+    
+}
+
+// esporto la funzione
+
+module.exports = hobbies;
---
+++ hobbies_js/script.js
@@ -0,0 +1,7 @@
+// scrivo la costante con require 
+
+const hobbies = require("./function.js");
+
+// do i valori agli hobby
+
+console.log(hobbies("giocare ai videogames", "leggere manga", "ascoltare musica"));
